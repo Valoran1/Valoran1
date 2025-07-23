5e36d412fab85092a50215991ea0e918e3fc554b
Valoran1 <valoran.project1@gmail.com>
2025-07-23 10:55:44 +0200

    Update chat.js

--- chat.js
+++ chat.js
@@ -33,7 +33,6 @@ exports.handler = async function(event, context) {
       statusCode: 200,
       body: JSON.stringify({ reply: data.choices[0].message.content })
     };
-
   } catch (error) {
     console.error("Function error:", error.message);
     return {
@@ -42,3 +41,4 @@ exports.handler = async function(event, context) {
     };
   }
 };
+
